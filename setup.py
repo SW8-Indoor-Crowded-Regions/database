e187ea766b1aa2402810174e6bad61c2105348b7
Morten Munk <mortenmunk97@gmail.com>
2025-03-04 10:59:44 +0100

    structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="indoor_crowded_region_detection_database",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here
+    ],
+    description="Database models for indoor region detection",
+    author="Group 1 - SW8",
+    url="https://github.com/SW8-Indoor-Crowded-Regions/database",
+)
