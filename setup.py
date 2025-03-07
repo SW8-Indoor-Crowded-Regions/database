d2dea8967cb02c4141b61fdcc1985b3dabe26839
Jack Skaarup Munn <92510884+JackSMunn@users.noreply.github.com>
2025-03-07 10:48:47 +0100

    updated setup.py
    - updated setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         "pymongo",
         "python-dotenv"
+        "mongoegine"
     ],
     description="Database models for indoor region detection",
     author="Group 1 - SW8",
