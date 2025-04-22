530d4592f9304734ab1b81bfc2c4a68f70616c56
Morten Munk <mortenmunk97@gmail.com>
2025-04-22 14:25:41 +0200

    hotfix for load_dotenv

--- db/database.py
+++ db/database.py
@@ -4,18 +4,16 @@ from mongoengine import connect
 
 
 class Database:
+	_connected = False
 
-    _connected = False
+	def __init__(self, uri=None):
+		if not Database._connected:
+			load_dotenv(override=True)
+			self.uri = uri if uri else os.getenv('MONGO_URL')
 
-    def __init__(self, uri=None):
+			if not self.uri:
+				raise ValueError('MONGO_URL is not set!')
 
-        if not Database._connected:
-            load_dotenv()
-            self.uri = uri if uri else os.getenv("MONGO_URL")
-
-            if not self.uri:
-                raise ValueError("MONGO_URL is not set!")
-
-            connect(host=self.uri)
-            Database._connected = True
-            print("✔️ Connected to MongoDB")
+			connect(host=self.uri)
+			Database._connected = True
+			print('✔️ Connected to MongoDB')
