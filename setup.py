63c2cde3b6503dd97f6b855f4c9cb3e7756c2375
Morten Munk <mortenmunk97@gmail.com>
2025-04-22 14:32:10 +0200

    updated version number for load_dotenv hotfix

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="indoor_crowded_region_detection_database",
-    version="1.0.7",
-    packages=find_packages(),
-    install_requires=[
-        "pymongo",
-        "python-dotenv",
-        "mongoengine"
-    ],
-    include_package_data=True,
-    package_data={
-        "": ["stubs/*.pyi"], 
-    },
-    description="Database models for indoor region detection",
-    author="Group 1 - SW8",
-    url="https://github.com/SW8-Indoor-Crowded-Regions/database",
+	name='indoor_crowded_region_detection_database',
+	version='1.0.8',
+	packages=find_packages(),
+	install_requires=['pymongo', 'python-dotenv', 'mongoengine'],
+	include_package_data=True,
+	package_data={
+		'': ['stubs/*.pyi'],
+	},
+	description='Database models for indoor region detection',
+	author='Group 1 - SW8',
+	url='https://github.com/SW8-Indoor-Crowded-Regions/database',
 )
